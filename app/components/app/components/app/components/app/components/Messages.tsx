5715767450a3ae4d559292416888e2cb37da8bd1
Gr33nstonecorp <gr33nstonecorp@gmail.com>
2026-03-13 11:34:47 -0400

    Delete app/components/app/components directory

--- app/components/app/components/app/components/app/components/Messages.tsx
+++
@@ -1,8 +0,0 @@
-export default function Messages() {
-  return (
-    <div>
-      <h2 className="text-3xl font-bold mb-4">Messages</h2>
-      <p>Your conversations will appear here.</p>
-    </div>
-  );
-}
