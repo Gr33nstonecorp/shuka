68b77ec89b4ea7b3a47c5908f26da48003800925
Gr33nstonecorp <gr33nstonecorp@gmail.com>
2026-03-13 16:17:49 -0400

    Update PhantomProvider.tsx

--- app/app/providers/PhantomProvider.tsx
+++ app/app/providers/PhantomProvider.tsx
@@ -1,1 +1,24 @@
+"use client";
 
+import { ReactNode } from "react";
+import { PhantomProvider } from "@phantom/react-sdk";
+import { AddressType } from "@phantom/browser-sdk";
+
+export default function ShukaPhantomProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <PhantomProvider
+      config={{
+        appId: "ba8bef06-7eef-4f29-bc09-e0d6313240df",
+        providers: ["injected"],
+        addressTypes: [AddressType.solana],
+      }}
+      appName="Shuka"
+    >
+      {children}
+    </PhantomProvider>
+  );
+}
