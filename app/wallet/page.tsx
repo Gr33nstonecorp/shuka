f989ed4701e939b7c43cbf5bd7ac63a6e1c1483a
Gr33nstonecorp <gr33nstonecorp@gmail.com>
2026-03-13 13:40:18 -0400

    Add WalletMultiButton to WalletPage component

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 export default function WalletPage() {
+
   return (
     <main style={{ padding: "40px" }}>
       <h1>Wallet</h1>
+      <p>Connect Phantom to use Shuka</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <WalletMultiButton />
+      </div>
+
     </main>
   );
 }
