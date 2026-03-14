a5a63ce41b7d1a710aef388b5c0df3a4d8061504
Gr33nstonecorp <gr33nstonecorp@gmail.com>
2026-03-14 13:56:34 -0400

    Create page.tsx

---
+++ app/receiving/page.tsx
@@ -0,0 +1,8 @@
+export default function ReceivingPage() {
+  return (
+    <main style={{ padding: "32px" }}>
+      <h1>Receiving</h1>
+      <p>Confirm delivered items and update inventory counts.</p>
+    </main>
+  );
+}
